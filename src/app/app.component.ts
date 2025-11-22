3b426bedafbbf94a66ddcd9d73fc0615dc8fdfbd
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-11-22 11:21:10 +0100

    add basic deps

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+const imports = [RouterOutlet];
+
+@Component({
+  selector: 'echo-root',
+  template: ` <router-outlet /> `,
+  imports,
+})
+export class App {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+/* eslint-disable no-console */
+import { App } from './app/app.component';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { bootstrapApplication } from '@angular/platform-browser';
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));

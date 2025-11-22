7bfac997c04fd6876ec6b4d0e7db47a651067e14
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-11-22 12:49:57 +0100

    feat: provide custom icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideEchoCustomIcons } from '#core/provider';
+
 import { routes } from '.';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [provideEchoCustomIcons(), provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };

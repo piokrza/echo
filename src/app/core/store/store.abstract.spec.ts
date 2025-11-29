6700a4c095c637227da938f70e8eeabf4595a243
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-11-29 14:45:15 +0100

    test(store): add unit tests

---
+++ src/app/core/store/store.abstract.spec.ts
@@ -0,0 +1,55 @@
+import { Store } from './store.abstract';
+
+import { Injectable, isSignal } from '@angular/core';
+
+interface TestStoreState {
+  isProcessing: boolean;
+  testThemeColor: 'white' | 'black';
+}
+
+const initialStateValue: TestStoreState = {
+  isProcessing: false,
+  testThemeColor: 'white',
+};
+
+@Injectable({ providedIn: 'root' })
+class TestStore extends Store<TestStoreState> {
+  constructor() {
+    super(initialStateValue);
+  }
+}
+
+describe('Store', () => {
+  let service: TestStore;
+
+  beforeEach(() => {
+    service = new TestStore();
+  });
+
+  it('should return initial value', () => {
+    expect(service.state()).toBe(initialStateValue);
+  });
+
+  it('should return state as Signal', () => {
+    expect(isSignal(service.state)).toBe(true);
+  });
+
+  it('should update value', () => {
+    service.update('testThemeColor', 'black');
+    expect(service.select('testThemeColor')()).toBe('black');
+    expect(service.state().testThemeColor).toBe('black');
+  });
+
+  it('should clear state', () => {
+    service.update('testThemeColor', 'black');
+    service.update('isProcessing', true);
+
+    service.clear();
+
+    expect(service.state()).toBe(initialStateValue);
+  });
+
+  it('should return state slice value', () => {
+    expect(service.select('testThemeColor')()).toBe(initialStateValue.testThemeColor);
+  });
+});

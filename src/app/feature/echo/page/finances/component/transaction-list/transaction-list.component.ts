e541bdde3f36d67d18598ee73427610f09663485
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-12-20 23:49:55 +0100

    feat(fincance): add basic structure

--- src/app/feature/echo/page/finances/component/transaction-list/transaction-list.component.ts
+++ src/app/feature/echo/page/finances/component/transaction-list/transaction-list.component.ts
@@ -7,7 +7,7 @@ const imports = [MatCardModule];
 @Component({
   selector: 'echo-transaction-list',
   template: `
-    <mat-card appearance="outlined" class="min-w-[200px] hover:bg-violet-600">
+    <mat-card appearance="outlined" class="min-w-[200px]">
       <mat-card-header>
         <mat-card-title>{{ heading() }}</mat-card-title>
       </mat-card-header>

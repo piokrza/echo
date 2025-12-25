b3dff9ddff310913c1655b4affa2bcb4f9a6e811
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-12-25 16:37:36 +0100

    feat(transaction-mobile-tile): update ui

--- src/app/feature/echo/page/finances/component/transaction-mobile-tile/transaction-mobile-tile.component.ts
+++ src/app/feature/echo/page/finances/component/transaction-mobile-tile/transaction-mobile-tile.component.ts
@@ -4,28 +4,36 @@ import { Component, computed, input, output } from '@angular/core';
 import { PrimeIcons } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { ChipModule } from 'primeng/chip';
+import { TagModule } from 'primeng/tag';
 
 import { EchoTransaction } from '#finances/model';
 import { TimestampToTextPipe } from '#ui/pipe';
 
-const imports = [CardModule, ButtonModule, TimestampToTextPipe, TitleCasePipe, ChipModule, CurrencyPipe];
+const imports = [CardModule, ButtonModule, TimestampToTextPipe, TitleCasePipe, TagModule, CurrencyPipe];
 
 @Component({
   selector: 'echo-transaction-mobile-tile',
   template: `
     <p-card tabindex="0" (click)="itemClick.emit(tx().id)" (keyup.space)="itemClick.emit(tx().id)">
-      <div class="flex justify-between items-center gap-4 mb-2">
-        <div class="flex items-center gap-3">
-          <i [class]="txIcon() + ' ' + (isIncome() ? 'color-success' : 'color-danger')"></i>
-          <p [class]="['text-xl', isIncome() ? 'color-success' : 'color-danger']">{{ tx().name | titlecase }}</p>
-        </div>
-        <p-chip [icon]="isIncome() ? PrimeIcons.ARROW_UP : PrimeIcons.ARROW_DOWN" [label]="(tx().amount | currency: 'USD') ?? ''" />
-      </div>
+      <div class="flex justify-between items-center">
+        <div>
+          <div class="flex justify-between items-center gap-4 mb-2">
+            <div class="flex items-center gap-3">
+              <i [class]="txIcon() + ' ' + (isIncome() ? 'color-success' : 'color-danger')"></i>
+              <p class="text-xl">{{ tx().name | titlecase }}</p>
+            </div>
+          </div>
 
-      <div class="flex items-center gap-2">
-        <p-chip class="p-2" [label]="tx().type | titlecase" />
-        <p>{{ tx().txDate | timestampToText }}</p>
+          <div class="flex items-center gap-2">
+            <p-tag
+              [severity]="isIncome() ? 'success' : 'danger'"
+              [value]="(tx().amount | currency: 'USD') ?? ''"
+              [icon]="isIncome() ? PrimeIcons.ARROW_UP : PrimeIcons.ARROW_DOWN" />
+            <p>{{ tx().txDate | timestampToText }}</p>
+          </div>
+        </div>
+
+        <i [class]="PrimeIcons.CHEVRON_RIGHT"></i>
       </div>
     </p-card>
   `,

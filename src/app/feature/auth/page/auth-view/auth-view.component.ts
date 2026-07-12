3d8aac179cffb28726748d9d18028ef6cf5b50f6
Piotr Krzaczkowski <slimhannya@gmail.com>
2026-07-12 12:21:38 +0200

    migrate auth-view

--- src/app/feature/auth/page/auth-view/auth-view.component.ts
+++ src/app/feature/auth/page/auth-view/auth-view.component.ts
@@ -1,54 +1,50 @@
 import { ChangeDetectionStrategy, Component, DestroyRef, inject, Signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
-import { PrimeIcons } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
 
 import { MatButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
 
 import { AuthFormComponent } from '#auth/component/auth-form';
 import { AuthForm, AuthFormType, AuthViewState } from '#auth/model';
 import { AuthViewService } from '#auth/service';
 
-const imports = [ButtonModule, MatButton, MatIcon, AuthFormComponent, CardModule, RouterLink];
+const imports = [MatButton, MatIcon, MatCardModule, AuthFormComponent, RouterLink];
 
 @Component({
   selector: 'echo-auth-dashboard',
   template: `
     <section class="grid place-items-center h-full echo-gradient px-4 bg-primary">
-      <div class="w-full grid place-items-center max-w-lg">
-        <p-card class="w-full">
-          <div class="grid">
-            <h2 class="text-2xl mb-8">Hey! Welcome back</h2>
+      <mat-card appearance="outlined" class="w-full max-w-lg">
+        <mat-card-header>
+          <mat-card-title>Hey! Welcome back</mat-card-title>
+        </mat-card-header>
 
+        <mat-card-content>
+          <div class="grid mt-8">
             <button matButton="outlined" [disabled]="state().isPerformingGoogleAuth" (click)="loginWithGoogle()">
               <mat-icon aria-hidden="false" aria-label="Google logo" svgIcon="google" />
               Login with Google
             </button>
 
-            <p-button
-              class="wide"
-              size="large"
-              severity="info"
-              label="Login with Google"
-              [outlined]="true"
-              [icon]="PrimeIcons.GOOGLE"
-              [loading]="state().isPerformingGoogleAuth"
-              (click)="loginWithGoogle()" />
             <span class="text-center my-4">or</span>
-
-            <echo-auth-form
-              [formType]="state().formType"
-              [isPerforming]="state().isPerformingEmailAndPasswordAuth"
-              (toggleFormType)="setFormType($event)"
-              (formSubmit)="authenticateWithEmailAndPassword($event)" />
-
-            <button pButton link class="w-fit mt-6" [icon]="PrimeIcons.ARROW_LEFT" [text]="true" [routerLink]="['']">Back to Echo</button>
           </div>
-        </p-card>
-      </div>
+
+          <echo-auth-form
+            [formType]="state().formType"
+            [isPerforming]="state().isPerformingEmailAndPasswordAuth"
+            (toggleFormType)="setFormType($event)"
+            (formSubmit)="authenticateWithEmailAndPassword($event)" />
+        </mat-card-content>
+
+        <mat-card-actions>
+          <button matButton="text" link class="w-fit mt-6" [routerLink]="['']">
+            <mat-icon aria-hidden="false" aria-label="Google logo" fontIcon="arrow_left_alt" />
+            Back to Echo
+          </button>
+        </mat-card-actions>
+      </mat-card>
     </section>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -59,8 +55,6 @@ export class AuthViewComponent {
   readonly #authViewService = inject(AuthViewService);
 
   readonly state: Signal<AuthViewState> = this.#authViewService.state;
-
-  readonly PrimeIcons = PrimeIcons;
 
   loginWithGoogle(): void {
     this.#authViewService.loginWithGoogle$().pipe(takeUntilDestroyed(this.#destroyRef)).subscribe();
